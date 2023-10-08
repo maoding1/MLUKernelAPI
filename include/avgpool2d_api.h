41d90150e89ca0e1237e594ddeec4ee2aea87069
mikeMao <1358489456@qq.com>
2023-10-08 16:23:49 +0800

    modify avgpool

--- include/avgpool2d_api.h
+++ include/avgpool2d_api.h
@@ -1,4 +1,4 @@
-void avgpool2d_mlu(float* dst, float* src, int channel,
+void avgpool2d_mlu(float* dst, float* src, int number,int channel,
                     int height, int width,
                     int kernel_height, int kernel_width,
                     int stride_width, int stride_height);
--- test/avgpool2d_test.cpp
+++ test/avgpool2d_test.cpp
@@ -5,9 +5,10 @@
 #ifndef AVGPOOL_TEST
 #define AVGPOOL_TEST
 
-#define CHANNEL 1
-#define HEIGHT 4
-#define WIDTH 4
+#define NUMBER 1
+#define CHANNEL 16
+#define HEIGHT 8
+#define WIDTH 8
 #define KERNEL_HEIGHT 2
 #define KERNEL_WIDTH 2
 #define STRIDE_WIDTH 2
@@ -15,7 +16,7 @@
 #endif
 
 int main() {
-    int test_len = 16;
+    int test_len = NUMBER * CHANNEL * HEIGHT * WIDTH;
     float* host_dst = (float*)malloc(test_len * sizeof(float));
     float* host_src = (float*)malloc(test_len * sizeof(float));
 
@@ -23,9 +24,11 @@ int main() {
     for(int i = 0; i < test_len; i++) {
         host_src[i] = i+1;
     }
-    avgpool2d_mlu(host_dst, host_src, CHANNEL, HEIGHT, WIDTH, KERNEL_HEIGHT, KERNEL_WIDTH, STRIDE_WIDTH, STRIDE_HEIGHT);
+    avgpool2d_mlu(host_dst, host_src, NUMBER, CHANNEL, HEIGHT, WIDTH, KERNEL_HEIGHT, KERNEL_WIDTH, STRIDE_WIDTH, STRIDE_HEIGHT);
 
-    for (int i = 0; i < 4; i++) {
+    int output_h = (HEIGHT - KERNEL_HEIGHT) / STRIDE_HEIGHT + 1;
+    int output_w = (WIDTH  - KERNEL_WIDTH) / STRIDE_WIDTH + 1;
+    for (int i = 0; i < NUMBER * CHANNEL * output_h * output_w; i++) {
         printf("%f ", host_dst[i]);
     }
 }
